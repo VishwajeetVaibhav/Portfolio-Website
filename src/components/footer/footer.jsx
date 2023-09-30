5613e7c0e23670d382128560835bc92d7f13b3d9
VishwajeetVaibhav <vishwajeetvaibhavv@gmail.com>
2023-09-30 22:21:24 +0530

    updated somethin in footer component

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <section id='footer'>footer</section>
+    <section id='footer'>Base</section>
   )
 }
 
-export default footer
+export default footer 
