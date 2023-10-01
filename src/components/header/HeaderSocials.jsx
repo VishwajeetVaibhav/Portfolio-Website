1d1e712d8f34645f6adbd61f8100847ff56d301e
VishwajeetVaibhav <vishwajeetvaibhavv@gmail.com>
2023-10-01 15:44:54 +0530

    Final Commit

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {BsInstagram} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href='http://linkedin.com' target='_blank'><BsLinkedin/></a>
-        <a href='http://github.com' target='_blank'><FaGithub/></a>
-        <a href='http://instagram.com' target='_blank'><BsInstagram/></a>
+        <a href='https://www.linkedin.com/in/vishwajeet-vaibhav-120362167/' target='_blank'><BsLinkedin/></a>
+        <a href='https://github.com/VishwajeetVaibhav' target='_blank'><FaGithub/></a>
+        <a href='https://www.instagram.com/vaibhavvishwajeet/' target='_blank'><BsInstagram/></a>
     </div>
   )
 }
